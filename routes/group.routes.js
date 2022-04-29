3d5ec16b674b41cb759c2e4b06ae02a35eb8de6e
i-benjelloun <ibenjelloun93@gmail.com>
2022-04-29 19:31:14 +0200

    Completed group CRUD

--- routes/group.routes.js
+++ routes/group.routes.js
@@ -1,11 +1,26 @@
-const { getGroups, createGroup } = require('../controllers/group.controllers');
+const {
+  getGroups,
+  createGroup,
+  deleteGroup,
+  getGroupById,
+  updateGroup,
+} = require('../controllers/group.controllers');
 const { isAuthenticated } = require('../middlewares/jwt.middlewares');
 const router = require('express').Router();
 
 // Route : get all groups where the user is a member
 router.get('/', isAuthenticated, getGroups);
 
-// Route : create a group
+// Route : create group
 router.post('/', isAuthenticated, createGroup);
+
+// Route : delete group
+router.delete('/:groupId', isAuthenticated, deleteGroup);
+
+// Route : delete group
+router.get('/:groupId', isAuthenticated, getGroupById);
+
+// Route : update group
+router.patch('/:groupId', isAuthenticated, updateGroup);
 
 module.exports = router;
