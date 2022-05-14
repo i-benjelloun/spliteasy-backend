14258056086859b9c03982ef87b41d886c9836d8
i-benjelloun <ibenjelloun93@gmail.com>
2022-05-14 21:49:01 +0200

    Validate group id

--- middlewares/isGroupMember.middlewares.js
+++ middlewares/isGroupMember.middlewares.js
@@ -1,14 +1,19 @@
+const { isValidId } = require('../helpers/isValidId');
 const Group = require('../models/Group.model');
 
 exports.isGroupMember = async (req, res, next) => {
   const current_user = req.payload._id;
   const { groupId } = req.params;
-  const group = await Group.findById(groupId);
-  if (group.members.includes(current_user)) {
-    next();
+  if (!isValidId(groupId)) {
+    return res.status(404).json({ errorMessage: 'Resource does not exist' });
   } else {
-    return res
-      .status(403)
-      .json({ errorMessage: 'Unauthorized access to resource' });
+    const group = await Group.findById(groupId);
+    if (group.members.includes(current_user)) {
+      next();
+    } else {
+      return res
+        .status(403)
+        .json({ errorMessage: 'Unauthorized access to resource' });
+    }
   }
 };
